90335d7f601cc161497e2b824db3f95f532edfa4
tshieta@gmail.com <tshieta@gmail.com>
2018-11-24 18:17:16 +0200

    project structure

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// Constants
+require_once 'functions.php';
+define('URLROOT', 'http://localhost/Sign_in-Sign_out-and-profile-management-with-PHP_MySQL-DB/');
+define('APPROOT', dirname(__FILE__));
+
+
+//Database Credentials 
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'nc_profilesys');
+
+
+
+
+require_once 'functions.php';
+
+
+
+
+
+
+
+
+?>
+
